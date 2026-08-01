c14512130f3ae961dcee237eccfd330cb1103e07
zelalem3 <zgetnet24@gmail.com>
2026-08-01 13:27:01 +0300

    frontend(api) : implemented token bearer to the request in the backend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -3,7 +3,19 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
-        'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
+});
+
+api.interceptors.request.use((config) => {
+    // Only set Content-Type to application/json if it's not already defined (allows form-data overrides like URLSearchParams)
+    if (!config.headers['Content-Type']) {
+        config.headers['Content-Type'] = 'application/json';
+    }
+
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
