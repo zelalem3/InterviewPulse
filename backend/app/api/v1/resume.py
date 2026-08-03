38f9230f4fb67478211cf37108b44e5bf331c5d1
zelalem3 <zgetnet24@gmail.com>
2026-08-03 14:11:06 +0300

    feat(backend): integrated Gemini Api for the skills extraction from the resume uploaded

--- backend/app/api/v1/resume.py
+++ backend/app/api/v1/resume.py
@@ -4,10 +4,11 @@ from app.models.models import Resume, User
 from app.schemas.resume import ResumeResponse
 from sqlalchemy.orm import Session
 import os
-import shutil  # <--- Added missing import
+import shutil 
 from app.core.deps import get_current_user
 from pypdf import PdfReader
 from typing import List
+import google.generativeai as genai
 
 
 router = APIRouter(prefix="/resume", tags=["resume"])
@@ -44,11 +45,13 @@ async def upload_resume(
                 parsed_text += text + "\n"
     except Exception:
         parsed_text = "Could not parse text automatically."
+    extract_skills = extract_text(parsed_text)
         
     new_resume = Resume(
         user_id=current_user.id,
         file_path=file_path,
-        parsed_content=parsed_text.strip()
+        parsed_content=parsed_text.strip(),
+        extracted_data = extract_skills
     )
     
     db.add(new_resume)
@@ -65,3 +68,25 @@ async def list_resumes(
 ):
     resumes = db.query(Resume).filter(Resume.user_id == current_user.id).all()
     return resumes
+
+
+def extract_text(parsed_text):
+    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+    
+    # Use an active current model string
+    model = genai.GenerativeModel("gemini-3.6-flash")
+    
+    prompt = f"""
+    I want you to extract the text, experience, projects, and education of the CV 
+    and give me the output back strictly in a JSON format.
+    
+    CV Content:
+    {parsed_text}
+    """
+    
+    response = model.generate_content(
+        prompt,
+        generation_config={"response_mime_type": "application/json"}
+    )
+    
+    return response.text
