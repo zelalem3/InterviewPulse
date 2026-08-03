b1eb7a344bcc3d30b6553cba0cc0e5c66e5ed523
zelalem3 <zgetnet24@gmail.com>
2026-08-03 14:24:24 +0300

    feat: integrate registration page into app router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,14 @@ import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import NewInterview from './pages/NewInterview';
 import InterviewRoom from './pages/InterviewRoom';
+import Register from './pages/RegisterPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/interview/new" element={<NewInterview />} />
         <Route path="/interview/:id" element={<InterviewRoom />} />
