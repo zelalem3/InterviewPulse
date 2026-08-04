f4f24a2897ad77ad18a2fac028d8c4ef2d439bcb
zelalem3 <zgetnet24@gmail.com>
2026-08-04 22:02:00 +0300

    feat(frontend): implemented a 404 page

---
+++ frontend/src/pages/PageNotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export function PageNotFound (){
+    return <>
+    <div>
+        <h1>Page Not Found</h1>
+        <a href="/">Go Back to the Dashboard</a>
+        
+    </div>
+    
+    </>
+}
