44a57b46cb0c66b4b0ef99bf15eb5f4acd36af95
zelalem3 <zgetnet24@gmail.com>
2026-08-07 09:25:35 +0300

    implemented protected route in the react-router for rerouting unauthorized user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,51 @@
 // src/App.tsx
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import NewInterview from './pages/NewInterview';
 import InterviewRoom from './pages/InterviewRoom';
 import Register from './pages/RegisterPage';
 import { CodeEditor } from './pages/CodeEditor';
+import { InterviewHistory } from './pages/InterviewHistory';
+import { PageNotFound } from './pages/PageNotFound';
+import { InterviewResult } from './pages/InterviewResult';
+import Navbar from './components/Navbar';
+import InterviewPage from './pages/InterviewPage';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function Layout() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-slate-100 flex flex-col">
+      
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          {/* --- PUBLIC ROUTES --- */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* --- PROTECTED ROUTES --- */}
+          <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/interview/new" element={<ProtectedRoute><NewInterview /></ProtectedRoute>} />
+          <Route path="/interview/:id" element={<ProtectedRoute><InterviewRoom /></ProtectedRoute>} />
+          <Route path="/:interview_id/editor" element={<ProtectedRoute><CodeEditor /></ProtectedRoute>} />
+          <Route path="/interview/history" element={<ProtectedRoute><InterviewHistory /></ProtectedRoute>} />
+          <Route path="/interview/:id/result" element={<ProtectedRoute><InterviewResult /></ProtectedRoute>} />
+          <Route path="/videointerivew" element={<ProtectedRoute><InterviewPage /></ProtectedRoute>} />
+
+          {/* --- 404 --- */}
+          <Route path="*" element={<PageNotFound />} /> 
+        </Routes>
+      </main>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/interview/new" element={<NewInterview />} />
-        <Route path="/interview/:id" element={<InterviewRoom />} />
-        <Route path="/editor" element={<CodeEditor />} />
-
-      </Routes>
+      <Layout />
     </BrowserRouter>
   );
 }
