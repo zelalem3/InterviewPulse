98e4e057dccaff73259b560d527c11021b9285c8
zelalem3 <zgetnet24@gmail.com>
2026-09-17 12:09:23 +0300

    feat(frontend): installed and configured a ssl plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),basicSsl()],
+
 })
